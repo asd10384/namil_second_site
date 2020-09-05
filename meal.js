adf9a3e99c40f6b58a2c1be38297fc4f91bdcf7e
asd10384 <65000786+asd10384@users.noreply.github.com>
2020-09-05 17:38:33 +0900

    12

---
+++ meal.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+const url = "http://school.busanedu.net/bsnamil-h/main.do";
+
+request(url, (error, response, body) => {
+  if (error) throw error;
+
+  let $ = cheerio.load(body);
+
+  try {
+    let jkcal = '';
+    let jmeal = '';
+
+    $('div.widgDiv.meal_menu1095').find('dl').each(function (index, elem) {
+    jkcal = $(this).find('dt').text().trim();
+    console.log(`${jkcal}`);
+
+    jmeal = $(this).find('dd').text().trim();
+    console.log(`${jmeal}`);
+
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
