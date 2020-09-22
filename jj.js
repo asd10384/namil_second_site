131b54f3d92189affe25b860804d48d3f92fd042
asd10384 <65000786+asd10384@users.noreply.github.com>
2020-09-22 22:30:34 +0900

    12

--- jj.js
+++ jj.js
@@ -1,15 +1,15 @@
 // browserify.cmd jj.js -o meal.js
 
-import request from 'request';
-import { load } from 'cheerio';
-import { writeFileSync } from 'fs';
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 const url = "http://bsnamil.hs.kr/bsnamil-h/main.do";
 
 request(url, (error, _response, body) => {
     if (error) throw error;
 
-    let $ = load(body);
+    let $ = cheerio.load(body);
 
     try {
         let jkcal = '';
@@ -25,7 +25,7 @@ request(url, (error, _response, body) => {
             const jskcal = `${jkcal}`
             const jsmeal = `${jmeal}`
 
-            writeFileSync("meal.txt", jskcal + "!" + jsmeal, {encoding: 'utf8'});
+            fs.writeFileSync("meal.txt", jskcal + "!" + jsmeal, {encoding: 'utf8'});
         });
     } catch (error) {
         console.error(error);
