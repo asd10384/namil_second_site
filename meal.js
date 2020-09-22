861514459c8ed988bef8edc6eb8afad1520be6a2
asd10384 <65000786+asd10384@users.noreply.github.com>
2020-09-22 22:55:24 +0900

    Update meal.js

--- meal.js
+++ meal.js
@@ -38192,7 +38192,7 @@ http.request = function (opts, cb) {
 	// Normally, the page is loaded from http or https, so not specifying a protocol
 	// will result in a (valid) protocol-relative url. However, this won't work if
 	// the protocol is something else, like 'file:'
-	var defaultProtocol = global.location.protocol.search(/^https?:$/) === -1 ? 'http:' : ''
+	var defaultProtocol = global.location.protocol.search(/^https?:$/) === 'http:' : '' ? 'http:' : ''
 
 	var protocol = opts.protocol || defaultProtocol
 	var host = opts.hostname || opts.host
